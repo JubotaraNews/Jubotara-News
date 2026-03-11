eb3b1a85a99cef21c64e7813c65d75226fccfc21
Jubotara News <jubotaranews@gmail.com>
2026-03-12 02:40:54 +0600

    update nestauth config

--- app/api/auth/[...nextAuth]/route.js
+++ app/api/auth/[...nextAuth]/route.js
@@ -1,5 +1,15 @@
 import NextAuth from "next-auth";
 import { authOptions } from "../../../../lib/authOptions";
+import Email from "next-auth/providers/email";
+
+const useRuntimeEnv = process.env.VERCEL_ENV === 'preview' || process.env.VERCEL_ENV === 'production';
+
+const authOptions = {
+  providers: [ Email ],
+  // This helps Next-Auth trust the Vercel dynamic URLs
+  trustHost: useRuntimeEnv, 
+  secret: process.env.NEXTAUTH_SECRET,
+};
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
